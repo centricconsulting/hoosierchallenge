ca45026ec5a5262812f8f46e090830df2af19ad2
Mark Stetzer <stetzer@gmail.com>
2013-07-19 08:12:44 -0400

    First commit

---
+++ project/Build.scala
@@ -0,0 +1,22 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "ccd-merge"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    anorm,
+    "ca.uhn.hapi" % "hapi" % "2.1"
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
