9550cbe6a448c3b6570a9fdea948a29a308b652c
Mark Stetzer <mark.stetzer@centricconsulting.com>
2013-07-29 14:00:33 -0400

    Coffee shop progress

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,29 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import service.DroolsCCDMerge
 
 object Application extends Controller {
-  
+
+  val mergeForm = Form(
+    tuple(
+      "ccd1" -> Byte[],
+      "ccd2" -> Byte[]
+    )
+  )
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def mergeDocs = Action {
+    mergeForm.bindFromRequest().get
+
+    val merged = DroolsCCDMerge.mergeDocs()
+    Ok(views.html.merged())
   }
   
 }
