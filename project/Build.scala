f6363fb5bfc6d16b0c7e638dcabdbeced342760e
TheNuttyArchitect <ben@techtriumphs.com>
2013-10-13 20:20:06 -0400

    begining to wire in securesocial to work with goolge auth as a demo

--- project/Build.scala
+++ project/Build.scala
@@ -15,13 +15,16 @@ object ApplicationBuild extends Build {
     "org.drools" % "drools-core" % "5.5.0.Final",
     "org.drools" % "drools-compiler" % "5.5.0.Final",
     "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
-    "securesocial" %% "securesocial" % "master-SNAPSHOT"
+    "commons-codec" % "commons-codec" % "1.8",
+    "org.apache.httpcomponents" % "httpclient" % "4.3",
+    "com.micronautics" % "securesocial" % "2.2.0" withSources
   )
 
 
-  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+  val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here
-      resolvers += Resolver.url("sbt-plugin-snapshots", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-snapshots/"))(Resolver.ivyStylePatterns)
+    resolvers += "Rhinofly Internal Release Repository" at "http://maven-repository.rhinofly.net:8081/artifactory/libs-release-local",
+    resolvers += Resolver.url("play-plugin-releases", new URL("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
   )
 
 }
