169d1efa8495730c54b95e8c5bfc833ca95779e0
TheNuttyArchitect <ben@techtriumphs.com>
2013-10-16 19:48:01 -0400

    more securesocial

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,18 +9,19 @@ import play.api.mvc._
 import service.DroolsCCDMerge
 import model.Transaction
 import util.CCDHelper
+import securesocial.core.{Identity, Authorization}
 
-object Application extends Controller {
+object Application extends Controller with securesocial.core.SecureSocial {
 
-  def index = Action {
+  def index = SecuredAction { implicit request =>
     Ok(views.html.index())
   }
 
-  def adhoc = Action {
+  def adhoc = SecuredAction { implicit request =>
     Ok(views.html.adhoc())
   }
 
-  def downloadTransactionDocument(id : Long, title : String) = Action { request =>
+  def downloadTransactionDocument(id : Long, title : String) = SecuredAction { implicit request =>
     import org.apache.commons.io.IOUtils
     val documents = Transaction.getTransactionDocumentAsStream(id)
     documents.map { is =>
@@ -32,7 +33,7 @@ object Application extends Controller {
     }.getOrElse(NotFound("File not found!"))
   }
 
-  def auditTrail = Action {
+  def auditTrail = SecuredAction { implicit request =>
     val transactions = Transaction.findLatestTransactions()
 
     val grouped = transactions.groupBy(tx => tx.id)
@@ -40,7 +41,7 @@ object Application extends Controller {
     Ok(views.html.auditTrail(keys.map(grouped.get(_).get).toSeq))
   }
 
-  def mergeDocs = Action { request =>
+  def mergeDocs = SecuredAction { implicit request =>
     val postedFiles = request.body.asMultipartFormData.get.files
     val helpers = postedFiles.map(f => new CCDHelper(Files.readFile(f.ref.file)))
 
